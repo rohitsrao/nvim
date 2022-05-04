a7da1aa605f1db229154fcf80f1b2e003e0f870d
Rohit Rao <rohit@rohitsrao.com>
2022-05-04 22:51:19 +0200

    Added a comma

--- lua/user/fluttertools.lua
+++ lua/user/fluttertools.lua
@@ -38,7 +38,7 @@ require("flutter-tools").setup {
   closing_tags = {
     highlight = "Comment", -- highlight for the closing tag
     prefix = "//", -- character to use for close tag e.g. > Widget
-    enabled = true -- set to false to disable
+    enabled = true, -- set to false to disable
   },
   dev_log = {
     enabled = true,
