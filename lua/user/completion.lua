c7b257d21b76902c39bfa9e447eac52627db46fe
Rohit Rao <rohit@rohitsrao.com>
2022-10-19 16:32:01 +0200

    Removed documentation on cmp menu and removed description of nerd font logo

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -85,7 +85,7 @@ cmp.setup {
     format = function(entry, vim_item)
       -- Kind icons
       -- This concatonates the icons with the name of the item kind
-      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) 
+      vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
         luasnip = "[Snippet]",
@@ -104,9 +104,5 @@ cmp.setup {
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
-  },
-  window = {
-   -- completion = cmp.config.window.bordered(),
-   documentation = cmp.config.window.bordered(),
   },
 }
