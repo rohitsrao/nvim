19ee5995c203daf5d7d02c367a260ba0223d3744
Rohit Rao <rohit@rohitsrao.com>
2022-10-24 22:59:25 +0200

    Enabled color highlighting

--- lua/user/fluttertools.lua
+++ lua/user/fluttertools.lua
@@ -54,8 +54,8 @@ require("flutter-tools").setup {
   },
   lsp = {
     color = { -- show the derived colours for dart variables
-      enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-      background = false, -- highlight the background
+      enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+      background = true, -- highlight the background
       foreground = false, -- highlight the foreground
       virtual_text = true, -- show the highlight using virtual text
       virtual_text_str = "■", -- the virtual text character to highlight
