3905d5327f6cd0562b09a8782e8296a0133f05ad
Rohit Rao <rohit@rohitsrao.com>
2023-06-28 23:24:25 +0200

    Eliminated ignore_ft_on_setup

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -14,11 +14,6 @@ nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
